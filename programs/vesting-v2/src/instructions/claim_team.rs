960f411a40c8fab82c5d051a1d3836ca3098db40
fabiandiaz416 <diazfabian99@icloud.com>
2024-12-27 16:37:17 -0300

    set INITIAL_TEAM amount of tokens using same formula as claim_marketing.rs

--- programs/vesting-v2/src/instructions/claim_team.rs
+++ programs/vesting-v2/src/instructions/claim_team.rs
@@ -67,16 +67,16 @@ pub fn claim_team(ctx: Context<ClaimTokens>) -> Result<()> {
         return Err(ErrorCode::InvalidPdaTokenAccount.into());
     }
 
-    const MARKETING_INITIAL_RELEASE: u64 = 3_750_000; // 3.5 milllones con 9 decimales // 25% liberados inmediatamente
-    const QUARTERLY_RELEASE: u64 = 1_410_000; // Amount of toknes ot be released at the end of every quarter
+    const TEAM_INITIAL_RELEASE: u64 = 2_000_000; // 2 milllones con 9 decimales // 20% liberados inmediatamente
+    const QUARTERLY_RELEASE: u64 = 1_000_000; // Amount of toknes ot be released at the end of every quarter
     const TOTAL_VESTING_PERIOD: u64 = 24;
     const QUARTERS_IN_SECONDS: i64 = 60 * 60 * 24 * 30 * 3;
 
     let mut available_tokens: u64 = 0;
 
     if now >= start_time {
-        if released_tokens < MARKETING_INITIAL_RELEASE {
-            available_tokens = MARKETING_INITIAL_RELEASE;
+        if released_tokens < TEAM_INITIAL_RELEASE {
+            available_tokens = TEAM_INITIAL_RELEASE;
         }
     }
 
